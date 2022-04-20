f09bc6df8fcc330e6b2e734a17311da2af19e592
kerciel <abdou.fazul.97610@gmail.com>
2022-04-20 10:33:44 +0200

    gestion erreur deploiment

--- Projet_pizza_mama/Startup.cs
+++ Projet_pizza_mama/Startup.cs
@@ -44,7 +44,8 @@ namespace Projet_pizza_mama
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            if(true)
             {
                 app.UseDeveloperExceptionPage();
             }
